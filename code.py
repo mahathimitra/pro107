f5ce75dd24488443da6fc5d8b4e34b7d40e5f721
mahathimitra <87479486+mahathimitra@users.noreply.github.com>
2021-08-21 18:49:40 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import csv
+import plotly_express as px
+
+df = pd.read_csv('data.csv')
+
+m = df.groupby(['student_id', 'level'], as_index = False)['attempt'].mean()
+
+fig = px.scatter(m, x = 'student_id', y = 'level', size='attempt', color = 'attempt')
+fig.show()
